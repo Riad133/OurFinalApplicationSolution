927f14ec51378fa54415b631a9cedb64c83d5e65
riad <riad.nazrul@gmail.com>
2019-05-18 02:01:35 +0600

    Add Openiddict and configer Token base Authentication and Refresh token.

---
+++ API/RequestResponse/RequestViewModel/RegistrationRequestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.RequestResponse.RequestViewModel
+{
+    public class RegistrationRequestViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+       
+    }
+}
